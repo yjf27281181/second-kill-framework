e7871a4c539db00d8d83080708bd39036533f8e1
long may the sunshine <31409032+yjf27281181@users.noreply.github.com>
2018-12-15 20:24:23 -0800

    first commit

---
+++ src/main/java/edu/usc/secondkill/SecondKillApplication.java
@@ -0,0 +1,14 @@
+package edu.usc.secondkill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondKillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondKillApplication.class, args);
+    }
+
+}
+
